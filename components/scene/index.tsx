e9b64c46d9ce9b066de35716c9d059d591a07b64
Ryan James <rejames@gmail.com>
2022-10-29 00:16:41 +0100

    lighting and triangles

--- components/scene/index.tsx
+++ components/scene/index.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { useRef, useState } from "react";
-import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+import { CustomObject } from "components/custom-object.tsx";
+
+import type { Group, Mesh } from "three";
 
 export function Scene() {
   return (
@@ -14,13 +18,13 @@ const COLORS = ["blue", "orange", "red", "green"] as const;
 type Color = typeof COLORS[number];
 
 export function SceneContent() {
-  const groupRef = useRef<ThreeElements["mesh"]>(null);
-  const cubeRef = useRef<ThreeElements["mesh"]>(null);
+  const groupRef = useRef<Group>(null!);
+  const cubeRef = useRef<Mesh>(null!);
   const [sphereColor, setSphereColor] = useState<Color>("orange");
 
   useFrame((_, delta) => {
-    cubeRef.current!.rotation.y += 0.03 * delta * 60;
-    groupRef.current!.rotation.y += 0.01 * delta * 60;
+    cubeRef.current.rotation.y += 0.01 * delta * 60;
+    groupRef.current.rotation.y += 0.01 * delta * 60;
   });
 
   const handleChangeSphereColor = () => {
@@ -31,10 +35,12 @@ export function SceneContent() {
   };
   return (
     <>
+      <directionalLight position={[1, 2, 3]} intensity={1.1} />
+      <ambientLight intensity={0.3} />
       <group ref={groupRef}>
         <mesh position-x={-2} onClick={handleChangeSphereColor}>
           <sphereGeometry />
-          <meshBasicMaterial color={sphereColor} wireframe />
+          <meshStandardMaterial color={sphereColor} />
         </mesh>
         <mesh
           ref={cubeRef}
@@ -43,14 +49,15 @@ export function SceneContent() {
           scale={1.5}
         >
           <boxGeometry />
-          <meshBasicMaterial color="mediumpurple" />
+          <meshStandardMaterial color="mediumpurple" />
         </mesh>
       </group>
 
       <mesh position-y={-1} rotation-x={Math.PI * -0.5} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
+      <CustomObject />
     </>
   );
 }
